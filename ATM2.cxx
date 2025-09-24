fe42fa531fb6c28a564f381bdd30f293e23e95e4
stingerBoo <sgrabinski@parasoft.com>
2025-09-24 13:33:47 +0200

    add file ATM2.cxx
    by stingerBoo

---
+++ ATM2.cxx
@@ -0,0 +1,16 @@
+#include "ATM.hxx"
+#include "BaseDisplay.hxx"
+
+ATM::ATM(Bank* bank, BaseDisplay* display)
+{
+    myBank = bank;
+    myDisplay = display;
+}
+
+void ATM::viewAccount(int accountNumber, string password)
+{
+    if ( !(myCurrentAccount = myBank->getAccount(accountNumber, password)) )
+    {
+        myDisplay->showInfoToUser("Invalid account");
+    }
+}
